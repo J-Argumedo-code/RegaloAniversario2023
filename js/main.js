2981f864bb2a503e06e28c8e51a7de602230b638
Josue Argumedo <josuargumedo@gmail.com>
2023-03-14 00:33:17 -0600

    Pre load faded animation fixed

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 window.addEventListener("load", ()=>{
+    document.getElementById("preLoad").style.transition = "2s";
     document.getElementById("preLoad").style.filter = "opacity(0)";
     document.getElementById("preLoad").style.zIndex = "-100";
 });
